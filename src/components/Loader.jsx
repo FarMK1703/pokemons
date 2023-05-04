17eef554ea89e21c0885a7d7ddfcb27ee87c7304
Farrukh Makhkamov <makhkamovfar@gmail.com>
2023-05-04 19:25:26 +0500

    version 1.0.2

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Loader() {
  return (
-    <div style={{width:'100%',height:'100%',display:'flex', alignItems:'center', justifyContent:'center'}}>
+    <div style={{width:'100vh',height:'100vh',display:'flex', alignItems:'center', justifyContent:'center'}}>
       <div class="lds-facebook"><div></div><div></div><div></div></div>
     </div>
   )
